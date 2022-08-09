73751063e96b07d8d2b03572ac8271763680a63b
Ryann1908 <59339990+RyannThi@users.noreply.github.com>
2022-08-09 11:42:50 -0300

    DFYDFYSDYSYDYT

--- My project/Assets/ControlePlayer.cs
+++ My project/Assets/ControlePlayer.cs
@@ -5,6 +5,13 @@ using static UnityEngine.InputSystem.InputAction;
 
 public class ControlePlayer : MonoBehaviour
 {
+
+    float sentido = 0; // o nome desse sujeito é campo, que é uma variavel declarada no escopo da classe
+
+    [Range(0f, 6f)]
+    [SerializeField]
+    float forcaPulo = 4f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,13 +21,24 @@ public class ControlePlayer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Vector2 velAtual = GetComponent<Rigidbody2D>().velocity;
+        velAtual.x = 5f * sentido;
+        GetComponent<Rigidbody2D>().velocity = velAtual;
     }
 
     public void Sentido(CallbackContext context)
     {
         //Debug.Log(context.ReadValue<float>());
-        transform.Translate(0.5f * context.ReadValue<float>(), 0, 0);
+        sentido = context.ReadValue<float>();
+        //transform.Translate(0.5f * context.ReadValue<float>(), 0, 0);
+    }
+
+    public void Pular(CallbackContext context)
+    {
+        if (context.ReadValue<float>() == 1)
+        {
+            GetComponent<Rigidbody2D>().AddForce(new Vector2(0, forcaPulo), ForceMode2D.Impulse);
+        }
     }
 
 }
