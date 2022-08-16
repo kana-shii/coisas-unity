fce06d64db841c0f62cb881fccf4c9f823d052f2
Ryann1908 <59339990+RyannThi@users.noreply.github.com>
2022-08-16 11:56:39 -0300

    att

--- My project/Assets/ControlePlayer.cs
+++ My project/Assets/ControlePlayer.cs
@@ -7,10 +7,16 @@ public class ControlePlayer : MonoBehaviour
 {
 
     float sentido = 0; // o nome desse sujeito é campo, que é uma variavel declarada no escopo da classe
+    const float RAIO_JUMPABLE = 0.5f;
 
-    [Range(0f, 6f)]
+    [Range(0f, 20f)]
     [SerializeField]
-    float forcaPulo = 4f;
+    float forcaPulo = 8f;
+
+    [SerializeField]
+    LayerMask layerMask;
+
+    bool noChao = false;
 
     // Start is called before the first frame update
     void Start()
@@ -22,8 +28,31 @@ public class ControlePlayer : MonoBehaviour
     void Update()
     {
         Vector2 velAtual = GetComponent<Rigidbody2D>().velocity;
-        velAtual.x = 5f * sentido;
+        velAtual.x = 8f * sentido;
         GetComponent<Rigidbody2D>().velocity = velAtual;
+        GetComponent<Animator>().SetBool("GROUNDED", noChao);
+        if (sentido != 0)
+        {
+            gameObject.transform.localScale = new Vector3(0.7f * sentido, 0.7f, 0.7f);
+            GetComponent<Animator>().SetBool("WALKING", true);
+        } else
+        {
+            GetComponent<Animator>().SetBool("WALKING", false);
+        }
+
+        
+    }
+
+    private void FixedUpdate()
+    {
+        Collider2D hit = Physics2D.OverlapCircle(transform.position, RAIO_JUMPABLE, layerMask);
+        if (hit != null)
+        {
+            noChao = true;
+        } else
+        {
+            noChao = false;
+        }
     }
 
     public void Sentido(CallbackContext context)
@@ -38,6 +67,9 @@ public class ControlePlayer : MonoBehaviour
         if (context.ReadValue<float>() == 1)
         {
             GetComponent<Rigidbody2D>().AddForce(new Vector2(0, forcaPulo), ForceMode2D.Impulse);
+            GetComponent<Animator>().SetTrigger("JUMP");
+            //GetComponent<Animator>().SetBool("GROUNDED", false);
+            noChao = false;
         }
     }
 
