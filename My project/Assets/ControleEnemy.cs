e86e4b739f1aa8fd37f26029f42bed293e4ab26e
Ryann1908 <59339990+RyannThi@users.noreply.github.com>
2022-08-23 11:48:06 -0300

    vilao e pulo consertado

---
+++ My project/Assets/ControleEnemy.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControleEnemy : MonoBehaviour
+{
+    [SerializeField]
+    LayerMask layerMask;
+
+    Rigidbody2D rigidbody;
+    Animator animator;
+
+    [SerializeField]
+    float alturaRaio, distanciaRaio;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Vector3 ajustaAltura = new Vector3(distanciaRaio / 2, alturaRaio, 0);
+        RaycastHit2D hit = Physics2D.Raycast(transform.position + ajustaAltura, Vector2.left, distanciaRaio, layerMask);
+        if (hit.collider != null)
+        {
+            if (transform.position.x > hit.point.x)
+            {
+                transform.localScale = new Vector3(-1, 1, 1);
+            } else
+            {
+                transform.localScale = new Vector3(1, 1, 1);
+            }
+            
+            rigidbody.velocity = new Vector2(transform.localScale.x, 0);
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.blue;
+        Vector3 posInicial= transform.position + new Vector3(distanciaRaio / 2, alturaRaio, 0);
+        Vector3 posFinal = transform.position + new Vector3(-distanciaRaio / 2, alturaRaio, 0);
+        
+        Gizmos.DrawLine(posInicial, posFinal);
+    }
+}
--- My project/Assets/ControlePlayer.cs
+++ My project/Assets/ControlePlayer.cs
@@ -7,7 +7,9 @@ public class ControlePlayer : MonoBehaviour
 {
 
     float sentido = 0; // o nome desse sujeito é campo, que é uma variavel declarada no escopo da classe
-    const float RAIO_JUMPABLE = 0.5f;
+    const float RAIO_JUMPABLE = 0.05f;
+    int puloVezes = 1;
+    int puloMax = 2; // somar
 
     [Range(0f, 20f)]
     [SerializeField]
@@ -18,26 +20,30 @@ public class ControlePlayer : MonoBehaviour
 
     bool noChao = false;
 
+    Animator animator;
+    Rigidbody2D rigidbody;
+
     // Start is called before the first frame update
     void Start()
     {
-
+        animator = GetComponent<Animator>();
+        rigidbody = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector2 velAtual = GetComponent<Rigidbody2D>().velocity;
+        Vector2 velAtual = rigidbody.velocity;
         velAtual.x = 8f * sentido;
-        GetComponent<Rigidbody2D>().velocity = velAtual;
-        GetComponent<Animator>().SetBool("GROUNDED", noChao);
+        rigidbody.velocity = velAtual;
+        animator.SetBool("GROUNDED", noChao);
         if (sentido != 0)
         {
             gameObject.transform.localScale = new Vector3(0.7f * sentido, 0.7f, 0.7f);
-            GetComponent<Animator>().SetBool("WALKING", true);
+            animator.SetBool("WALKING", true);
         } else
         {
-            GetComponent<Animator>().SetBool("WALKING", false);
+            animator.SetBool("WALKING", false);
         }
 
         
@@ -46,9 +52,12 @@ public class ControlePlayer : MonoBehaviour
     private void FixedUpdate()
     {
         Collider2D hit = Physics2D.OverlapCircle(transform.position, RAIO_JUMPABLE, layerMask);
-        if (hit != null)
+        Debug.Log(rigidbody.velocity.y);
+        if (hit != null && (rigidbody.velocity.y <= 0.5f || puloVezes == 0))
         {
             noChao = true;
+            animator.SetBool("JUMP", false);
+            puloVezes = 0;
         } else
         {
             noChao = false;
@@ -64,12 +73,13 @@ public class ControlePlayer : MonoBehaviour
 
     public void Pular(CallbackContext context)
     {
-        if (context.ReadValue<float>() == 1)
+        if (context.ReadValue<float>() == 1 && puloVezes <= puloMax)
         {
-            GetComponent<Rigidbody2D>().AddForce(new Vector2(0, forcaPulo), ForceMode2D.Impulse);
-            GetComponent<Animator>().SetTrigger("JUMP");
+            rigidbody.AddForce(new Vector2(0, forcaPulo), ForceMode2D.Impulse);
+            animator.SetBool("JUMP", true);
             //GetComponent<Animator>().SetBool("GROUNDED", false);
             noChao = false;
+            puloVezes += 1;
         }
     }
 
